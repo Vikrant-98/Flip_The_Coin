d27d935790676af994477e9d095d24ea620a8f90
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-26 02:53:48 +0530

    Winning combination added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash -x
 
-heads_Side=1
-tails_Side=0
+Heads_Side=1
+Tails_Side=0
 count[0]=0
 while [ ${count[0]} != 100 ]
 do
@@ -10,37 +9,37 @@ do
 	count_fix1=0
 	((count[0]++))
 	side_Of_Coin=$((RANDOM%2))
-	if [[ $side_Of_Coin == $heads_Side ]]
+	if [[ $side_Of_Coin == $Heads_Side ]]
 	then
 		while [ $count_fix != 1 ]
 		do
 			((count_fix++))
         		side_Of_Coin=$((RANDOM%2))
-        		if [[ $side_Of_Coin == $heads_Side ]]
+        		if [[ $side_Of_Coin == $Heads_Side ]]
         		then
                 		while [ $count_fix1 != 1 ]
 		                do
                         		((count_fix1++))
                         		side_Of_Coin=$((RANDOM%2))
-                        			if [[ $side_Of_Coin == $heads_Side ]]
+                        			if [[ $side_Of_Coin == $Heads_Side ]]
                         			then
                                 			((count[1]++))
-                        			elif [[ $side_Of_Coin == $tails_Side ]]
+                        			elif [[ $side_Of_Coin == $Tails_Side ]]
                         			then
                                 			((count[2]++))
                         			fi
                 		done
 
-        		elif [[ $side_Of_Coin == $tails_Side ]]
+        		elif [[ $side_Of_Coin == $Tails_Side ]]
         		then
                 		while [ $count_fix1 != 1 ]
                 		do
                         		((count_fix1++))
                         		side_Of_Coin=$((RANDOM%2))
-                        			if [[ $side_Of_Coin == $heads_Side ]]
+                        			if [[ $side_Of_Coin == $Heads_Side ]]
                         			then
                                 			((count[3]++))
-                        			elif [[ $side_Of_Coin == $tails_Side ]]
+                        			elif [[ $side_Of_Coin == $Tails_Side ]]
                         			then
                                 			((count[4]++))
                         			fi
@@ -49,37 +48,37 @@ do
         		fi
 		done
 
-	elif [[ $side_Of_Coin == $tails_Side ]]
+	elif [[ $side_Of_Coin == $Tails_Side ]]
 	then
 		while [ $count_fix != 1 ]
 		do
 			((count_fix++))
         		side_Of_Coin=$((RANDOM%2))
-        		if [[ $side_Of_Coin == $heads_Side ]]
+        		if [[ $side_Of_Coin == $Heads_Side ]]
         		then
                 		while [ $count_fix1 != 1 ]
                 		do
                         		((count_fix1++))
                         		side_Of_Coin=$((RANDOM%2))
-                        			if [[ $side_Of_Coin == $heads_Side ]]
+                        			if [[ $side_Of_Coin == $Heads_Side ]]
                         			then
                                 			((count[5]++))
-                        			elif [[ $side_Of_Coin == $tails_Side ]]
+                        			elif [[ $side_Of_Coin == $Tails_Side ]]
                         			then
                                 			((count[6]++))
                         			fi
                 		done
 
-        		elif [[ $side_Of_Coin == $tails_Side ]]
+        		elif [[ $side_Of_Coin == $Tails_Side ]]
         		then
                 		while [ $count_fix1 != 1 ]
                 		do
                         		((count_fix1++))
                         		side_Of_Coin=$((RANDOM%2))
-                        			if [[ $side_Of_Coin == $heads_Side ]]
+                        			if [[ $side_Of_Coin == $Heads_Side ]]
                         			then
                                 			((count[7]++))
-                        			elif [[ $side_Of_Coin == $tails_Side ]]
+                        			elif [[ $side_Of_Coin == $Tails_Side ]]
                         			then
                                 			((count[8]++))
                         			fi
@@ -111,3 +110,82 @@ percentage[Tail_Head_Head]=$( echo "scale=2; ${count[5]}/${count[0]}*100 " | bc 
 percentage[Tail_Head_Tail]=$( echo "scale=2; ${count[6]}/${count[0]}*100 " | bc )
 percentage[Tail_Tail_Head]=$( echo "scale=2; ${count[7]}/${count[0]}*100 " | bc )
 percentage[Tail_Tail_Tail]=$( echo "scale=2; ${count[8]}/${count[0]}*100 " | bc )
+
+index=1
+record=$index
+greater=${count[index]}
+while [[ $index -le 7 ]]
+do
+
+	if [[ greater -lt ${count[index+1]} ]]
+	then
+		greater=${count[index+1]}
+		record=$(($index+1))
+	fi
+((index++))
+done
+case $record in
+
+	1)
+		echo "Head_Head_Head comes most of the time"
+		;;
+	2)
+		echo "Head_Head_Tail comes most of the time"
+		;;
+	3)
+		echo "Head_Tail_Head comes most of the time"
+		;;
+	4)
+		echo "Head_Tail_Tail comes most of the time"
+		;;
+	5)
+		echo "Tail_Head_Head comes most of the time"
+		;;
+	6)
+		echo "Tail_Head_Tail comes most of the time"
+		;;	
+	7)
+		echo "Tail_Tail_Head comes most of the time"
+		;;
+	8)
+		echo "Tail_Tail_Tail comes most of the time"
+		;;
+	*)
+		;;
+esac
+index=11
+record=$index
+greater=${count[index]}
+while [[ $index -le 13 ]]
+do
+
+	if [[ greater -lt ${count[index+1]} ]]
+	then
+		greater=${count[index+1]}
+		record=$(($index+1))
+	fi
+((index++))
+done
+case $record in
+
+	11)
+		echo "Head_Head comes most of the time"
+		;;
+	12)
+		echo "Head_Tail comes most of the time"
+		;;
+	13)
+		echo "Tail_Head comes most of the time"
+		;;
+	14)
+		echo "Tail_Tail comes most of the time"
+		;;
+	*)
+		;;
+esac
+if [[ ${count[9]} -gt ${count[10]} ]]
+then
+	echo "Head comes most of the time"
+else
+	echo "Tail comes most of the time"
+fi
