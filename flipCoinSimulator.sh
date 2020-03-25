3807d13abb8c302993a9350bb3fe88ccec5e423b
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-26 02:34:31 +0530

    Doublet combination of coin added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,21 +5,48 @@ tails_Side=0
 count=0
 count1=0
 count2=0
+count3=0
+count4=0
 
 while [ $count != 100 ]
 do
-((count++))
-side_Of_Coin=$((RANDOM%2))
-if [[ $side_Of_Coin == $heads_Side ]]
-then
-	((count1++))
-	echo "Heads"
-elif [[ $side_Of_Coin == $tails_Side ]]
-then
-	((count2++))
-	echo "Tails"
-fi
+	count_fix=0
+	((count++))
+	side_Of_Coin=$((RANDOM%2))
+	if [[ $side_Of_Coin == $heads_Side ]]
+	then
+		while [ $count_fix != 1 ]
+		do
+			((count_fix++))
+        		side_Of_Coin=$((RANDOM%2))
+        		if [[ $side_Of_Coin == $heads_Side ]]
+        		then
+                		((count1++))
+        		elif [[ $side_Of_Coin == $tails_Side ]]
+        		then
+                		((count2++))
+        		fi
+		done
+
+	elif [[ $side_Of_Coin == $tails_Side ]]
+	then
+		while [ $count_fix != 1 ]
+		do
+			((count_fix++))
+        		side_Of_Coin=$((RANDOM%2))
+        		if [[ $side_Of_Coin == $heads_Side ]]
+        		then
+                		((count3++))
+        		elif [[ $side_Of_Coin == $tails_Side ]]
+        		then
+                		((count4++))
+        		fi
+		done
+
+	fi
 done
 declare -A percentage
-percentage[singlet_Head]=$( echo "scale=2; $count1/$count*100 " | bc )
-percentage[singlet_Tail]=$( echo "scale=2; $count2/$count*100 " | bc )
+percentage[Head_Head]=$( echo "scale=2; $count1/$count*100 " | bc )
+percentage[Head_Tail]=$( echo "scale=2; $count2/$count*100 " | bc )
+percentage[Tail_Head]=$( echo "scale=2; $count3/$count*100 " | bc )
+percentage[Tail_Tail]=$( echo "scale=2; $count4/$count*100 " | bc )
